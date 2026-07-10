7da0d4917348d3ba1592dbbd9e9e4419156bcae3
lsouzesss-lgtm <lsouzesss@gmail.com>
2026-07-09 23:52:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
+const botao = document.getElementById("entrar");
 
+botao.addEventListener("click", () => {
+    alert("Bem-vindo ❤️\n\nEste é apenas o começo de um presente feito especialmente para você.");
+});
