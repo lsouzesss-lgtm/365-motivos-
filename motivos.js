eaed0ccd16bd8a56a3bfe997b3719a3e73e98df8
lsouzesss-lgtm <lsouzesss@gmail.com>
2026-07-09 23:53:55 -0300

    Update motivos.js

--- motivos.js
+++ motivos.js
@@ -1,1 +1,7 @@
-
+const motivos = [
+    "Porque você faz meu coração se sentir em casa.",
+    "Porque seu abraço é meu lugar favorito.",
+    "Porque você sempre consegue me fazer sorrir.",
+    "Porque eu escolheria você em todas as vidas.",
+    "Porque amar você é a coisa mais fácil do mundo."
+];
